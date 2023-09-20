51170e37f68b701298c05aff5094a8f6833e9f4c
hendiksaputra <hendiksaputra04@gmail.com>
2023-09-20 10:34:36 +0800

    feat: Create Basic Structure

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,23 @@
-import './globals.css'
+import MainLayout from "@/components/MainLayout";
+import "./globals.css";
+import MenuContextProvider from "@/context/MenuContext";
 // import { Inter } from 'next/font/google'
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Next.js + Tailwind CSS',
-  description: 'Project Tutorial',
-}
+  title: "Next.js + Tailwind CSS",
+  description: "Project Tutorial",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MenuContextProvider>
+          <MainLayout>{children}</MainLayout>
+        </MenuContextProvider>
+      </body>
     </html>
   );
 }
