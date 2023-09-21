951082f5906ae4a105e615b0672c9b622ad51398
hendiksaputra <hendiksaputra04@gmail.com>
2023-09-21 09:59:00 +0800

    feat: add LanguageSelectBox

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      
     </div>
   );
 };
---
+++ components/layout/LanguageSelectBox.jsx
@@ -0,0 +1,56 @@
+"use client";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { BsGlobeAmericas } from "react-icons/bs";
+
+const languages = [
+  { id: 1, flag: "fi fi-us", route: "/us" },
+  { id: 2, flag: "fi fi-mc", route: "/mc" },
+];
+
+const LanguageSelectBox = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center relative z-30">
+        <div
+          onClick={() => setOpen((prev) => !prev)}
+          className="p-2 cursor-pointer"
+        >
+          <span className="text-2xl">
+            <BsGlobeAmericas />
+          </span>
+        </div>
+        <div
+          className={`flex flex-col bg-orange-100 w-16 rounded-lg ${
+            open ? "opacity-100` h-auto" : "opacity-0 h-0"
+          } transition-all duration-200 overflow-hidden absolute top-12 right-8`}
+        >
+          {languages.map((item) => (
+            <div
+              key={item}
+              onClick={() => {
+                setOpen(false);
+              }}
+              className={`flex justify-start items-center p-2 py-1 hover:bg-orange-300 cursor-pointer`}
+            >
+              <Link href={item.route}>
+                <span className={item.flag}></span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div
+        onClick={() => setOpen(false)}
+        className={`bg-gray-100 fixed inset-0 opacity-50 z-0 ${
+          open ? "block" : "hidden"
+        }`}
+      ></div>
+    </>
+  );
+};
+
+export default LanguageSelectBox;
