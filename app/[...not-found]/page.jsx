61cf8c0b011bcd6de798b42397d848eb316932a5
hendiksaputra <hendiksaputra04@gmail.com>
2023-09-21 09:26:03 +0800

    feat: add General NotFound Page

---
+++ app/[...not-found]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="bg-green-200">
+      <h1>NotFoundPage</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
